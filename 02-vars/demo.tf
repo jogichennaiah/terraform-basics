3d690fd6a249447f15ad40a419648d9791757b6e
jogichennaiah <chennaiahjogi394@gmail.com>
2023-09-30 13:24:25 +0530

    Auto push

--- 02-vars/demo.tf
+++ 02-vars/demo.tf
@@ -32,5 +32,4 @@ variable "sample_list" {
 
 output "print_list {
   value      = " Welcome To ${var.sample_list[0]} Trainig and the batch number is ${var.sample_list[1]} and the name is ${var.sample_list[3]}"
- 
 }
