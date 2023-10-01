2a8b814ead4288781dd9ead4c009ff28e5ba3e47
jogichennaiah <chennaiahjogi394@gmail.com>
2023-10-01 13:33:30 +0530

    Auto push

--- 04-remote-state/provider.tf
+++ 04-remote-state/provider.tf
@@ -2,9 +2,9 @@ provider "aws" {}
 
 terraform {
     backend "s3" {
-        bucket = "b55-terraform-bucket"
-        key    = "dev/ec2/terraform.tfstate"
-        region = "us-east-1" 
+        bucket  = "b55-terraform-bucket"
+        key     = "dev/ec2/terraform.tfstate"
+        region  = "us-east-1" 
 
     }
  }
