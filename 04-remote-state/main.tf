bcc2ba014e4b6135ff5ec97695d9b4cff53e930a
jogichennaiah <chennaiahjogi394@gmail.com>
2023-10-02 11:19:05 +0530

    Auto push

--- 04-remote-state/main.tf
+++ 04-remote-state/main.tf
@@ -1,7 +1,18 @@
+#Datasource to fetch the info of AMI
+
+data "aws_ami" "ami" {
+  most_recent      = true
+  name_regex       = "DevOps-LabImage-CentOS7"
+  owners           = ["self"]
+}
+ 
+
+
+
 resource "aws_instance" "web" {
   count         = var.howManyYouWant
 
-  ami           = "ami-0c1d144c8fdd8d690"
+  ami           = aws_ami.ami.image_id
   instance_type = "t3.medium"
 
   tags = {
