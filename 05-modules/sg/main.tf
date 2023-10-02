bfc777d08e5b964c5bca01bad00cc0b512edb110
jogichennaiah <chennaiahjogi394@gmail.com>
2023-10-02 14:43:39 +0530

    Auto push

--- 05-modules/sg/main.tf
+++ 05-modules/sg/main.tf
@@ -27,5 +27,5 @@ resource "aws_security_group" "allows_ssh" {
 
 #Step1 : Declare the info that you wish to share as OUTPUT
 output "sgid" {
-  value  = aws_security_group.allow_ssh.id
+  value  = aws_security_group.allows_ssh.id
 }
