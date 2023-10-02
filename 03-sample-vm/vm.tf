7f07cb4ce255e9614e0a4570a2164d7c226ec055
jogichennaiah <chennaiahjogi394@gmail.com>
2023-10-02 12:52:52 +0530

    Auto push

--- 03-sample-vm/vm.tf
+++ 03-sample-vm/vm.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "web" {
-  ami           = "ami-05377c442727e8308"
-  instance_type = "t3.medium"
+  ami                      = "ami-05377c442727e8308"
+  instance_type            = "t3.medium"
+  vpc_security_group_ids   = [aws_security_group.allow_ssh.id]
 
   tags = {
     Name = "My-Tf-Instance"
