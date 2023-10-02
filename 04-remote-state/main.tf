d1486e5cdc73b44d470ff1ef5534e83ea2464540
jogichennaiah <chennaiahjogi394@gmail.com>
2023-10-02 12:14:28 +0530

    Auto push

--- 04-remote-state/main.tf
+++ 04-remote-state/main.tf
@@ -22,3 +22,7 @@ resource "aws_instance" "web" {
 variable "instances" {
   default = ["catalogue" , "user" , "cart" , "shipping"]
 }
+
+output "private_ip_address" {
+  value   = aws_instance.web.*.private_dns
+}
